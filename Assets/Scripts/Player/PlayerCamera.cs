095d68b16e19f655b3ce9cddb1c3fc2bb90c9d7e
gesua <gesua@hanmail.net>
2018-08-02 09:56:21 +0900

    Revert "카메라 구현"
    This reverts commit 05e9a607cbb66b2b2b9a9edb78fa49cefefe8382.

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    // 화면 넘어갈 수 있는 크기
+    float MinX = -5;
+    float MaxX = 5;
+    float MinZ = -3;
+    float MaxZ = 3;
+
+    Transform target; // 플레이어 위치
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+        if (target == null)
+        {
+            Debug.LogError("Player 못 찾음");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 myPos = transform.position;
+        Vector3 targetPos = target.position;
+        Vector3 tempPos = Vector3.zero;
+
+        float tempX = myPos.x - targetPos.x;
+        float tempZ = myPos.z - targetPos.z;
+
+        Debug.Log(tempX + ", " + tempZ);
+
+        if(tempX < -MinX)
+        {
+            tempPos.x = targetPos.x - (myPos.x + Mathf.Abs(MinX));
+        }
+
+        //if (tempX > MaxX || tempX < MinX)
+        //{
+        //    tempPos.x = myPos.x - targetPos.x;
+        //}
+        //if(tempZ > MaxZ || tempZ < MinZ)
+        //{
+        //    tempPos.z = myPos.z - targetPos.z;
+        //}
+
+        transform.position += tempPos;
+    }
+}
