6766da8f65c2651782cf076a9557a0b22f795f62
ckdlscjs <ckdlscjs@naver.com>
2018-08-07 16:37:14 +0900

    패턴수정및 등등

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -27,14 +27,16 @@ public class Enemy : MonoBehaviour {
     Animator EnemyAnimator;
     //플레이어의 공격 시간 체크
     public float attackDelay = 1.5f;
-    //공격지속시간(attakDelay뺀시간)
-    public float attackContinuousTime = 6f;
     float currADTime = 0f;
+    //공격지속시간
+    float currCtTime = 0f;
+    public float attackContinuousTime = 3f;
     //공격상태체크
     bool attackState = false;
     //이동상태체크
     bool moveState = true;
     BulletSpawner bulletSpawner;
+    //BulletSpawner bulletSpawner;
     //캐릭터의 방향을 체크해줄 값
     bool checkRight = false;
     //플레이어의 위치를 받을값
@@ -42,7 +44,7 @@ public class Enemy : MonoBehaviour {
     //플레이어의방향계산
     Vector3 dir;
     //사용시간
-    float currTime = 0f;
+    float currTimeMov1 = 0f;
     //패턴종류
     //패턴종류랜덤지정
     int setRandomMove = 0;
@@ -54,10 +56,12 @@ public class Enemy : MonoBehaviour {
 
     ////////////////패턴2 ->지정방향으로 순간이동,순간이동지정시에 플레이어 위치 주변반경 일정범위 내에 랜덤으로 나옴
 
-    float currTimePat2 = 0f;
-    public float maxTime2 = 1.5f;
+    float currTimeMov2 = 0f;
+    public float Mov2SetTime = 1.5f;
     //사라졌다가 다시 나오는대기시간
-    public float respawnTime = 4.0f;
+    bool mov2ResCheck = false;
+    float currTimeMov2Res = 0f;
+    public float respawnTime = 1f;
     bool tempTel = false;
     Vector3 tempTelPos = Vector3.zero;
 
@@ -94,21 +98,43 @@ public class Enemy : MonoBehaviour {
         EnemyAnimator = gameObject.GetComponentInChildren<Animator>();
         blink = gameObject.GetComponent<DamagedBlink>();
         BossHp = GameObject.Find("Bar").GetComponentInChildren<Image>();
-        bulletSpawner = GameObject.Find("E_BulletSpawner").GetComponent<BulletSpawner>();
+        //bulletSpawner = GameObject.Find("E_BulletSpawner").GetComponent<BulletSpawner>();
         teleport = GameObject.Find("Teleport");
-        teleport.GetComponent<SpriteRenderer>().enabled = false;
+        //스프라이트지정
+        teleportSprite = teleport.GetComponent<SpriteRenderer>();
+        teleportSprite.enabled = false;
+        //밖으로빼낼거
         Sprite[] tempSprite = Resources.LoadAll<Sprite>("Sprites/telEffect");
+        int teleportCount = 0;
+        sprite = new Sprite[8];
+        for (int i = 0; i < tempSprite.Length; i++) {
+            if (tempSprite[i].name.Contains("teleport")) {
+                sprite[teleportCount++] = tempSprite[i];
+            }
+        }
     }
 
     // Update is called once per frame
     void Update() {
+        currTimeMov1 += Time.deltaTime;
+        currTimeMov2 += Time.deltaTime;
+        if (mov2ResCheck == true) {
+            currTimeMov2Res += Time.deltaTime;
+        }
+        if(attackState) {
+            currCtTime += Time.deltaTime;
+        }
+
 
         //행동지정
         SetEnemyBehavState();
         //공격패턴지정
         SetAttackPattern();
-        //무브패턴지정
-        SetMovePattern();
+        if (moveState) {
+            currADTime += Time.deltaTime;
+            //무브패턴지정
+            SetMovePattern();
+        }
 
 
         //애니메이터
@@ -116,47 +142,48 @@ public class Enemy : MonoBehaviour {
     }
 
     void SetEnemyBehavState() {
-        //무브상태체크
+        //상태체크
         if (currADTime > attackDelay) {
-            //공격시간재기
-            currADTime += Time.deltaTime;
             //상태변경
             attackState = true;
             moveState = false;
-            if (attackState) {
-                aniState = ANI_STATE.E_AP1;
-                //bulletSpawner.attackState = this.attackState;
+        }
+        if (attackState) {
+            //애니메이션지정필요(체력/공격패턴따라)
+            aniState = ANI_STATE.E_AP1;
+            //bulletSpawner.attackState = this.attackState;
+        }
+        if (currCtTime > attackContinuousTime) {
+            //시간끝 공격중지
+            //시간초기화
+            currADTime = 0;
+            currCtTime = 0;
+            //상태변경
+            attackState = false;
+            moveState = true;
+            //공격패턴0초기화
+            if (HP > 50) {
+                BulletSpawner.Instance.bulletState = 0;
             }
-            if (currADTime > attackContinuousTime) {
-                //시간끝 공격중지
-                currADTime = 0;
-                attackState = false;
-                moveState = true;
-                currTime = 0;
-                Debug.Log("위치재선정1");
-                setRandomMove = Random.Range(0, 100);
-                //bulletSpawner.attackState = this.attackState;
-
-                //공격패턴0초기화
-                if (HP > 50) {
-                    bulletSpawner.bulletState = 0;
-                }
-                else if (HP <= 50) {
-                    bulletSpawner.bulletState = 1;
-                }
+            else if (HP <= 50) {
+                BulletSpawner.Instance.bulletState = 1;
             }
         }
+
 
     }
 
     void SetMovePattern() {
+        //공격시간재기
+       
+        setRandomMove = Random.Range(0, 100);
+        //체력적을때 정해주는 패턴
         if (HP > 12) {
             if (setRandomMove > 50) {
                 movePattern = 0;
             }
             else {
                 //위치재선정재필요
-
                 movePattern = 1;
             }
         }
@@ -164,27 +191,27 @@ public class Enemy : MonoBehaviour {
             movePattern = 2;
         }
 
+        Move();
 
-        if (moveState) {
-            currADTime += Time.deltaTime;
-            Move();
-            //보스의이동
-            switch (movePattern) {
-                case 0:
-                    //플레이어따라감
-                    this.transform.position += dir * moveSpeed * Time.deltaTime;
-                    break;
-                case 1:
-                    MovePattern1();
-                    break;
+        //보스의이동
+        switch (movePattern) {
+            case 0:
+                //플레이어따라감
+                this.transform.position += dir * moveSpeed * Time.deltaTime;
+                break;
+            case 1:
+                MovePattern1();
+                break;
 
-                case 2:
-                    MovePattern2();
-                    break;
+            case 2:
+                MovePattern2();
+                break;
 
-            }
         }
+
     }
+
+
     void Move() {
         //방향계산
         dir = target.position - this.transform.position;
@@ -222,7 +249,7 @@ public class Enemy : MonoBehaviour {
         if (HP > 50) {
             if (currPatCheck < atPat1Check) {
                 if (onDamagedCount > CheckPat1Dam) {
-                    bulletSpawner.bulletState = 1;
+                    BulletSpawner.Instance.bulletState = 1;
                 }
             }
             else {
@@ -233,7 +260,7 @@ public class Enemy : MonoBehaviour {
         else if (HP <= 50) {
             if (currPatCheck < atPat2Check) {
                 if (onDamagedCount > CheckPat2Dam) {
-                    bulletSpawner.bulletState = 2;
+                    BulletSpawner.Instance.bulletState = 2;
                 }
             }
             else {
@@ -248,45 +275,44 @@ public class Enemy : MonoBehaviour {
     }
     void MovePattern1() {
         //패턴1의 시간 계산
-        currTime += Time.deltaTime;
+
         //시간이 0일때만 dir의 방향을 받음
-        if (currTime <= 0.5) {
+        if (currTimeMov1 <= 0.5) {
             tempPos = target.transform.position;
             tempDir = tempPos - this.transform.position;
         }
 
         this.transform.position += tempDir.normalized * moveSpeed * Time.deltaTime;
         if ((this.transform.position.x - 1 < tempPos.x && this.transform.position.x + 1 > tempPos.x) && (this.transform.position.z - 1 < tempPos.z && this.transform.position.z + 1 > tempPos.z)) {
-            currTime = 0;
+            currTimeMov1 = 0;
             Debug.Log("위치재선정");
         }
 
     }
     void MovePattern2() {
         //텔레포트
-        //임시지정
-        //애니메이션 필요(방향지정,슈웅사라짐)
-        currTimePat2 += Time.deltaTime;
-        if (currTimePat2 > maxTime2) {
+ 
+        if (currTimeMov2 > Mov2SetTime) {
             //해당위치에 나올지점 이펙트 찍어주기
             if (tempTel == false) {
                 tempTelPos = target.position;
-                Debug.Log(tempTelPos);
                 tempTel = true;
-                Debug.Log("순간이동시전,위치 기존이동");
                 //순간이동위치에 찍어주기
                 teleport.transform.position = tempTelPos;
-                teleport.GetComponent<SpriteRenderer>().enabled = true;
-                
+                teleportSprite.enabled = true;
+                mov2ResCheck = true;
+                StartCoroutine("teleportAlert");
             }
-            if (currTimePat2 > respawnTime) {
-                Debug.Log("순간이동대기시간끝,완료");
-                this.transform.position = tempTelPos;
-                currTimePat2 = 0;
-                tempTel = false;
-                teleport.GetComponent<SpriteRenderer>().enabled = false;
-                
-            }
+           
+        }
+        if (currTimeMov2Res > respawnTime) {
+            Debug.Log("순간이동대기시간끝,완료");
+            this.transform.position = tempTelPos;
+            currTimeMov2 = 0;
+            currTimeMov2Res = 0;
+            tempTel = false;
+            mov2ResCheck = false;
+            teleportSprite.enabled = false;
         }
     }
     void PlayAnimator() {
@@ -346,7 +372,23 @@ public class Enemy : MonoBehaviour {
 
     public void AllStop() {
         //총알 멈춰주는 작업이 필요함
-        bulletSpawner.AllBulletOff();
+        BulletSpawner.Instance.AllBulletOff();
         EnemyAnimator.enabled = false;
+    }
+
+    IEnumerator teleportAlert() {
+        //PlayerSprite.sprite = LoseSprites[0];
+
+        //for (int i = 1; i < 3; i++) {
+        //    yield return new WaitForSeconds(1f);
+        //    PlayerSprite.sprite = LoseSprites[i];
+        //}
+        //Shadow.gameObject.SetActive(false);
+        teleportSprite.sprite = sprite[0];
+        for (int i = 1; i < sprite.Length; i++) {
+            yield return new WaitForSeconds(0.15f);
+            teleportSprite.sprite = sprite[i];
+        }
+
     }
 }
