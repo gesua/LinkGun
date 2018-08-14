ed859a7448a09341517504410c1097e872b445af
gesua <gesua@hanmail.net>
2018-08-14 15:26:05 +0900

    아이템 완료

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -8,9 +8,14 @@ public class Item : MonoBehaviour
 
     Gun PlayerGun;
 
+    // 이펙트 반짝반짝
+    SpriteRenderer EffectSR;
+    int AddValue = 1;
+    float ChangeValue = 1;
+
     private void Start()
     {
-        if(ItemNumber == -1)
+        if (ItemNumber == -1)
         {
             Debug.LogError(gameObject.name + " 아이템 초기화 안 됨");
         }
@@ -22,6 +27,30 @@ public class Item : MonoBehaviour
             Debug.LogError("PlayerGun 못 찾음");
             return;
         }
+
+        EffectSR = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        if (EffectSR == null)
+        {
+            Debug.LogError("EffectSR 못 찾음");
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        // 반짝반짝
+        ChangeValue += AddValue * Time.deltaTime;
+
+        if (ChangeValue <= 0.5f)
+        {
+            AddValue = 1;
+        }
+        else if (ChangeValue >= 1f)
+        {
+            AddValue = -1;
+        }
+
+        EffectSR.color = new Color(1, 1, 1, ChangeValue);
     }
 
     // 아이템 획득
