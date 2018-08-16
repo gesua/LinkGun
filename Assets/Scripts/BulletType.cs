d8c798d32bc6589d49ecad93ecdee7f0ba9cfd36
gesua <gesua@hanmail.net>
2018-08-16 16:25:08 +0900

    탄 수정

--- Assets/Scripts/BulletType.cs
+++ Assets/Scripts/BulletType.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletType : MonoBehaviour {
-    public enum B_Type {
+public class BulletType : MonoBehaviour
+{
+    public enum B_Type
+    {
         Basic,
         Knife,
         Nabi,
@@ -31,41 +33,51 @@ public class BulletType : MonoBehaviour {
     int NabiCount = 0; // 스프라이트 몇 번째 보여줄지
     int NabiAddValue = 1; // 더해질 값
 
-    private void Awake() {
+    private void Awake()
+    {
         // 딱 1번만 스프라이트 찾음
-        if (IsInitialize == false) {
+        if (IsInitialize == false)
+        {
             // 총알 찾기
             Sprite[] temp = Resources.LoadAll<Sprite>("Sprites/BulletImage2");
             NabiSprite = new Sprite[4];
             int nabiCount = 0;
-            for (int i = 0; i < temp.Length; i++) {
+            for (int i = 0; i < temp.Length; i++)
+            {
                 // 기본
-                if (temp[i].name.Equals("Basic")) {
+                if (temp[i].name.Equals("Basic"))
+                {
                     BasicSprite = temp[i];
                 }
                 // 나이프
-                else if (temp[i].name.Equals("WhiteKnife")) {
+                else if (temp[i].name.Equals("WhiteKnife"))
+                {
                     KnifeSprite = temp[i];
                 }
                 // 나비
-                else if (temp[i].name.Contains("Nabi")) {
+                else if (temp[i].name.Contains("Nabi"))
+                {
                     NabiSprite[nabiCount] = temp[i];
                     nabiCount++;
                 }
                 // 수리검
-                else if (temp[i].name.Equals("Shuriken")) {
+                else if (temp[i].name.Equals("Shuriken"))
+                {
                     ShurikenSprite = temp[i];
                 }
             }
-            if (KnifeSprite == null) {
+            if (KnifeSprite == null)
+            {
                 Debug.LogError("Knife 못 찾음");
                 return;
             }
-            if (NabiSprite[NabiSprite.Length - 1] == null) {
+            if (NabiSprite[NabiSprite.Length - 1] == null)
+            {
                 Debug.LogError("Nabi 끝까지 못 찾음");
                 return;
             }
-            if (ShurikenSprite == null) {
+            if (ShurikenSprite == null)
+            {
                 Debug.LogError("ShurikenSprite 못 찾음");
                 return;
             }
@@ -75,21 +87,25 @@ public class BulletType : MonoBehaviour {
 
         // 랜더러 가져옴
         BulletSR = GetComponentInChildren<SpriteRenderer>();
-        if (BulletSR == null) {
+        if (BulletSR == null)
+        {
             Debug.LogError("BulletSR 못 찾음");
             return;
         }
 
         // 콜라이더 가져옴
         BulletCollider = GetComponent<BoxCollider>();
-        if (BulletCollider == null) {
+        if (BulletCollider == null)
+        {
             Debug.LogError("BulletCollider 못 찾음");
             return;
         }
     }
 
-    void OnEnable() {
-        switch (BType) {
+    void OnEnable()
+    {
+        switch (BType)
+        {
             case B_Type.Basic: // 기본
                 BulletSR.sprite = BasicSprite;
                 BulletCollider.center = Vector3.zero;
@@ -113,19 +129,23 @@ public class BulletType : MonoBehaviour {
                 break;
         }
         BulletSR.transform.localScale = new Vector3(3, 3, 1);
-        BulletSR.transform.rotation = Quaternion.Euler(new Vector3(90f, 0, 0));
+        BulletSR.transform.localEulerAngles = new Vector3(90f, 0, 0);
     }
 
-    private void Update() {
+    private void Update()
+    {
         // 움직이는 총알
-        if (IsAnimation) {
-            switch (BType) {
+        if (IsAnimation)
+        {
+            switch (BType)
+            {
                 // 나비
                 case B_Type.Nabi:
                     CurrentTime += Time.deltaTime;
 
                     // 애니메이션 재생
-                    if (CurrentTime >= 0.05f) {
+                    if (CurrentTime >= 0.05f)
+                    {
                         CurrentTime = 0; // 초기화
                         NabiCount += NabiAddValue; // 다음 스프라이트
 
@@ -147,12 +167,14 @@ public class BulletType : MonoBehaviour {
     }
 
     // 총알 타입 설정
-    public void SetBType(B_Type type) {
+    public void SetBType(B_Type type)
+    {
         BType = type;
     }
 
     // 애니메이션 끄기
-    public void StopAnimation() {
+    public void StopAnimation()
+    {
         IsAnimation = false;
     }
 }
