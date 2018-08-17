ee31a25e1d5cee688fb4fa6934611b8563ebc718
ckdlscjs <ckdlscjs@naver.com>
2018-08-17 13:43:36 +0900

    몹추가완료

--- Assets/Scripts/Enemy/EnemyMob.cs
+++ Assets/Scripts/Enemy/EnemyMob.cs
@@ -8,7 +8,7 @@ public class EnemyMob : MonoBehaviour {
     public float moveSpeed = 1f; //이동속도
     public int MaxHP = 50; //최대체력
     public int CurrHP = 0; //현재체력
-    public Transform target; //플레이어의 위치를 받을값
+    Transform target; //플레이어의 위치를 받을값
     bool checkRight = false; //오른쪽체크
     NavMeshAgent agent; //네비
     Rigidbody rigid; //충돌체 밀림방지
@@ -24,18 +24,24 @@ public class EnemyMob : MonoBehaviour {
     float currTime = 0f;
     public float b_spawnTime = 0.3f; //총알발사딜레이
     float b_currTime = 0f;
-    
 
-	// Use this for initialization
-	void Start () {
+    //이펙트
+    SpriteRenderer mobSprite;
+    Sprite[] sprites;
+
+    // Use this for initialization
+    void Start() {
         CurrHP = MaxHP; //HP초기화
-        //동적할당
+        //할당
+        mobSprite = gameObject.GetComponentInChildren<SpriteRenderer>(); //스프라이트지정
+        SetDeadSprite();
+        target = GameObject.Find("Player").transform;//캐릭터위치
         agent = gameObject.GetComponent<NavMeshAgent>();
         blink = gameObject.GetComponent<DamagedBlink>();
         rigid = gameObject.GetComponent<Rigidbody>(); //충돌체부착
         agent.speed = moveSpeed; //속도초기화
         agent.SetDestination(target.transform.position);//위치지정
-	}
+    }
 
     // Update is called once per frame
     void Update() {
@@ -47,7 +53,7 @@ public class EnemyMob : MonoBehaviour {
         }
 
         MoveDir();//방향전환
-        switch(attackState) {
+        switch (attackState) {
             case true:
                 //공격상태일때
                 Attack();
@@ -65,17 +71,18 @@ public class EnemyMob : MonoBehaviour {
             attackState = false;
             currTime = 0f;
             agent.speed = moveSpeed;
-        } else {
+        }
+        else {
             //공격하기
             b_currTime += Time.deltaTime; //시간계산
-            if(b_currTime > b_spawnTime) {
+            if (b_currTime > b_spawnTime) {
                 GameObject bullet = BulletSpawner.Instance.ShootBullet();
                 bullet.GetComponent<BulletType>().BType = BulletType.B_Type.Amulet;
                 bullet.transform.position = this.transform.position;
                 bullet.transform.LookAt(target.transform);
                 b_currTime = 0f;
             }
-            
+
         }
 
 
@@ -87,7 +94,8 @@ public class EnemyMob : MonoBehaviour {
             currTime = 0f;
             agent.speed = 0; //속도 0
             agent.velocity = Vector3.zero; //가속도0
-        } else {
+        }
+        else {
             agent.SetDestination(target.transform.position);//위치지정
 
         }
@@ -120,7 +128,33 @@ public class EnemyMob : MonoBehaviour {
         blink.BlinkStart();
 
         if (CurrHP <= 0) {
-            //죽었엉
+            gameObject.GetComponentInChildren<Animator>().enabled = false;
+            StartCoroutine("MobDeadEffect");
+            agent.speed = 0f;//멈추고
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            gameObject.GetComponent<SphereCollider>().enabled = false;
+        }
+    }
+    IEnumerator MobDeadEffect() {
+        Sprite tempSprite = mobSprite.sprite;//현재스프라이트받기(생존중)
+        mobSprite.sprite = sprites[0];
+        for (int i = 1; i < sprites.Length; i++) {
+            yield return new WaitForSeconds(0.15f);
+            mobSprite.sprite = sprites[i];
+        }
+        yield return new WaitForSeconds(0.1f);
+        MobSpawner.Instance.AddMobPool(gameObject);
+        mobSprite.sprite = tempSprite;
+    }
+
+    void SetDeadSprite() {
+        Sprite[] tempSprite = Resources.LoadAll<Sprite>("Sprites/YukDead");
+        int DeadCount = 0;
+        sprites = new Sprite[3];
+        for (int i = 0; i < tempSprite.Length; i++) {
+            if (tempSprite[i].name.Contains("Yuk")) {
+                sprites[DeadCount++] = tempSprite[i];
+            }
         }
     }
 }
---
+++ Assets/Scripts/Enemy/MobSpawner.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoSingleton<MobSpawner> {
+    //스포너위치를받을배열
+    Transform[] spawnPos;
+    //스폰시간
+    public float mobSpawnTime = 1f;
+    float spawnCurrTime = 0f;
+
+    //잡몹 오브젝트풀
+    public GameObject mobFactory;
+    GameObject[] mobPool;
+    List<GameObject> deactiveList = new List<GameObject>();
+    public int poolSize = 100;
+
+    private void Awake() {
+        //싱글톤
+        SetInstance(this);
+    }
+    // Use this for initialization
+    void Start() {
+        SetPool(); //풀지정
+        spawnPos = GetComponentsInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnCurrTime += Time.deltaTime;
+        if(spawnCurrTime > mobSpawnTime) {
+            if (deactiveList.Count > 0) {
+                spawnCurrTime = 0f;
+                //위치지정
+                GameObject tempMob = MobPoolActive();
+                tempMob.transform.position = spawnPos[Random.Range(1, spawnPos.Length)].position;
+            }
+            else {
+                Debug.Log("모든몹나옴");
+            }
+        }
+	}
+    //몹풀
+    void SetPool() {
+        mobPool = new GameObject[poolSize];
+        for (int i = 0; i < poolSize; i++) {
+            mobPool[i] = Instantiate(mobFactory);
+            mobPool[i].SetActive(false);
+            mobPool[i].transform.parent = GameObject.Find("E_Mob").transform;
+            deactiveList.Add(mobPool[i]);
+        }
+    }
+    
+    //풀액티브
+    GameObject MobPoolActive() {
+       
+        GameObject tempMob = deactiveList[0];
+        deactiveList.RemoveAt(0);
+        //꺼졌던 액티브 다시온
+        tempMob.GetComponentInChildren<Animator>().enabled = true;
+        tempMob.GetComponent<BoxCollider>().enabled = true;
+        tempMob.GetComponent<SphereCollider>().enabled = true;
+        tempMob.SetActive(true);
+        return tempMob;
+    }
+
+    public void AddMobPool(GameObject mob) {
+        mob.SetActive(false);
+        deactiveList.Add(mob);
+    }
+    public void AllMobOff() {
+        for (int i = 0; i < poolSize; i++) {
+            mobPool[i].GetComponent<EnemyMob>().enabled = false;
+        }
+    }
+}
