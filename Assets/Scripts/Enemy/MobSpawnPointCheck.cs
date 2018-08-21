5df0390f81de1cd7745cd2c6095d8f0c8193f02c
ckdlscjs <ckdlscjs@naver.com>
2018-08-21 09:50:30 +0900

    허허

--- Assets/Scripts/Enemy/MobSpawnPointCheck.cs
+++ Assets/Scripts/Enemy/MobSpawnPointCheck.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MobSpawnPointCheck : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		
