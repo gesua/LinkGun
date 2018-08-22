7c9907affde6782f8cec9cc46dce6f6bfeb24463
gesua <gesua@hanmail.net>
2018-08-22 11:20:09 +0900

    조작법 추가

---
+++ Assets/Scripts/Title/TextColor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextColor : MonoBehaviour
+{
+    static Color PrevColor; // 이전 색
+    static Color NextColor; // 다음 색
+    static int ColorCount = -1;
+
+    Text MyText;
+
+    private void Awake()
+    {
+        MyText = GetComponentInChildren<Text>();
+    }
+
+    void Update()
+    {
+        MyText.color = ColorChange();
+    }
+
+    Color RandomColor()
+    {
+        //float r = Random.Range(200f, 256f) / 255f;
+        //float g = Random.Range(200f, 256f) / 255f;
+        //float b = Random.Range(200f, 256f) / 255f;
+        float r = Random.value;
+        float g = Random.value;
+        float b = Random.value;
+
+        return new Color(r, g, b);
+    }
+
+    // 색 보간
+    Color ColorChange()
+    {
+        // 맨 처음에만 지정
+        if (ColorCount == -1)
+        {
+            PrevColor = RandomColor();
+            NextColor = RandomColor();
+            ColorCount++;
+        }
+
+        Color applyColor = Color.Lerp(PrevColor, NextColor, ColorCount / 20f);
+
+        // 새로운 색 지정
+        if (applyColor.Equals(NextColor))
+        {
+            PrevColor = NextColor;
+            NextColor = RandomColor();
+        }
+
+        // 카운트 초기화
+        if (ColorCount >= 20)
+        {
+            ColorCount = 0;
+        }
+        else
+        {
+            ColorCount++;
+        }
+
+        return applyColor;
+    }
+}
